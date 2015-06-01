8d8cf7ecbd3c17a92566f6d500eed11f9b5ef0ba
Marquina <marquinamarie@gmail.com>
2015-06-01 15:34:02 -0400

    Created ebooksite application attempt two

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ebooksite::Application.config.secret_key_base = '9cb3d194d57ec483a769e914cca692ea2e35e95d138c476fe416dc916e1ebcb86b0419a27067fae46fa5a75b83b2d6eabf97c2b0302c6b18fb999c823e1ce165'
